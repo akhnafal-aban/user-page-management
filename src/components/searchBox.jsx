e51b6af0251308a2293a48c92f7a75f9b83a3b33
AKHNAFAL <22523179@students.uii.ac.id>
2024-06-08 01:19:43 +0700

    test hapus

--- src/components/searchBox.jsx
+++ src/components/searchBox.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
-import './styles.css';
+// import React, { useState } from 'react';
+// import './styles.css';
 
-const SearchBox = ({ onSearch }) => {
-  return (
-    <input
-      type="text"
-      className="search-box"
-      placeholder="Search..."
-      onChange={(e) => onSearch(e.target.value)}
-    />
-  );
-};
+// const SearchBox = ({ onSearch }) => {
+//   return (
+//     <input
+//       type="text"
+//       className="search-box"
+//       placeholder="Search..."
+//       onChange={(e) => onSearch(e.target.value)}
+//     />
+//   );
+// };
 
-export default SearchBox;
+// export default SearchBox;
--- src/components/userList.jsx
+++ src/components/userList.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import './styles.css';
+// import React from 'react';
+// import './styles.css';
 
-const UserList = ({ users }) => (
-  <ul>
-    {users.map((user) => (
-      <li key={user.id} className="user-item">
-        <div><strong>ID:</strong> {user.id}</div>
-        <div><strong>Name:</strong> {user.name}</div>
-        <div><strong>Email:</strong> {user.email}</div>
-        <div><strong>Gender:</strong> {user.gender}</div>
-        <div><strong>Status:</strong> {user.status}</div>
-      </li>
-    ))}
-  </ul>
-);
+// const UserList = ({ users }) => (
+//   <ul>
+//     {users.map((user) => (
+//       <li key={user.id} className="user-item">
+//         <div><strong>ID:</strong> {user.id}</div>
+//         <div><strong>Name:</strong> {user.name}</div>
+//         <div><strong>Email:</strong> {user.email}</div>
+//         <div><strong>Gender:</strong> {user.gender}</div>
+//         <div><strong>Status:</strong> {user.status}</div>
+//       </li>
+//     ))}
+//   </ul>
+// );
 
-export default UserList;
+// export default UserList;
